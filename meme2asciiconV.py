4caf879ab97906fb1702486f8ebfa04043ddc0f1
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-08-06 21:38:17 +0530

    refacored code

--- meme2asciiconV.py
+++ meme2asciiconV.py
@@ -1,17 +1,17 @@
 import cv2
-from math import gcd
+from math import gcd, trunc
 from main import func
 
-while 1:  
-    imgPath = input('Image path: ')
+while True:  
+    image_path = input('please type the path of image: ')
     try:
-        img = cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2GRAY)
+        raw_img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2GRAY)
         break
     except cv2.error:
-        print('\nInvalid file path.\n')
+        print('\nInvalid file path, try again.\n')
 
-height, width = img.shape[:2]
+height, width = raw_img.shape[:2]
 image_size = gcd(height, width)
 
 if __name__ == "__main__":
-    func(height, width, image_size, img)
+    func(height, width, image_size, raw_img)
