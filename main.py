a5ad0b455e3f1b2c1412ee3a3ed01f8bb636a504
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-08-06 21:34:21 +0530

    refacored code

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from math import ceil
+
+
+def func(ht,wt,sz,img):
+    ascii_img = []
+    height = ht
+    width = wt
+    image_size = sz
+
+    for y in range(0, height, image_size):
+        newRow = []
+        for x in range(0, width, image_size):
+            remRow = 0
+            for y_axis in range(y, y + image_size):
+                for x_axis in range(x, x + image_size):
+                    remRow += img[y_axis][x_axis]
+            newRow.append(remRow / image_size ** 2)
+        ascii_img.append(newRow)
+
+    # Dark to ligheight
+    Light_set = reversed('`",:;Il!i~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$')
+    Dark_set = '@#%+-. '
+
+    colGrad = tuple(Dark_set)
+    interval = 255 / len(colGrad)
+
+    with open('output_image_text.txt', 'w') as out_file:
+        for row in ascii_img:
+            for gCol in row:
+                for m in range(len(colGrad)):
+                    if interval * m <= gCol <= interval * (m + 1):
+                        out_file.write(colGrad[m] * ceil(width / height))
+
+            out_file.write('\n')
+
+    print(f'Done! Output file has file path {os.path.abspath(os.getcwd())}\output_image_text.txt')
